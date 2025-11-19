b64d3254ef79cdc4cf49103ef267bb249c6a7ac0
Catalina Sfarlea <catalinamaria2803@gmail.com>
2025-11-19 12:44:26 +0200

    Add project files.

---
+++ SfarleaCatalinaLab7/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SfarleaCatalinaLab7")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SfarleaCatalinaLab7.Pages")]
