bd119dbf787e328a2d91623efa2e30e99f2a0378
Catalina Sfarlea <catalinamaria2803@gmail.com>
2025-11-26 18:18:24 +0200

    lab8

--- SfarleaCatalinaLab7/App.xaml.cs
+++ SfarleaCatalinaLab7/App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace SfarleaCatalinaLab7
+﻿using System;
+using SfarleaCatalinaLab7.Data;
+using System.IO;
+
+
+namespace SfarleaCatalinaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
