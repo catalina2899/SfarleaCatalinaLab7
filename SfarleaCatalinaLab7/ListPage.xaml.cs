486d345b2a7e71f28dc49db1df0a91a353b1177d
Catalina Sfarlea <catalinamaria2803@gmail.com>
2025-12-03 17:45:09 +0200

    lab9

--- SfarleaCatalinaLab7/ListPage.xaml.cs
+++ SfarleaCatalinaLab7/ListPage.xaml.cs
@@ -20,4 +20,46 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+    this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        var shopl = (ShopList)BindingContext;
+        var selectedProduct = listView.SelectedItem as Product;
+
+        if (selectedProduct == null)
+        {
+            await DisplayAlert("Warning",
+                               "Please select an item from the list first.",
+                               "OK");
+            return;
+        }
+
+        bool confirm = await DisplayAlert("Delete item",
+                                         $"Delete '{selectedProduct.Description}' from this shopping list?",
+                                         "Yes", "No");
+        if (!confirm)
+            return;
+
+        
+        await App.Database.DeleteListProductAsync(shopl.ID, selectedProduct.ID);
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 }
